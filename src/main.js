7c12e0dd1b334b615ec292344ecfdad52e6323bb
Diman-ko <dr.komarov@mail.ru>
2024-08-22 12:19:33 +0200

    feat: all wark

--- src/main.js
+++ src/main.js
@@ -2,11 +2,33 @@ const cols = document.querySelectorAll('.col')
 
 
 document.addEventListener('keydown', event => {
-    console.log(event.code)
+    event.preventDefault()
+    // console.log(event.code)
     if (event.code.toLowerCase() === "space") {
         setRenderColors()
     }
 })
+
+
+
+document.addEventListener("click", event => {
+    // console.log(event.target.dataset)
+    const type = event.target.dataset.type
+    if (type === 'lock') {
+        // console.log('perform lock')
+        const node = event.target.tagName.toLowerCase() === 'i'
+            ? event.target : event.target.children[0]
+        // console.log(node)
+        node.classList.toggle('fa-lock-open')
+        node.classList.toggle('fa-lock')
+    } else if (type === 'copy') {
+        copyToClickBoard(event.target.textContent)
+    }
+})
+
+function copyToClickBoard(text) {
+    return navigator.clipboard.writeText(text)
+}
 
 function generateRandomColor() {
     // RGB
@@ -17,12 +39,21 @@ function generateRandomColor() {
     }
     return '#' + color
 }
+
+
+
 function setRenderColors() {
-    cols.forEach(col => {
+    cols.forEach((col) => {
+        //определить является ли кнопка заблокированной
+        const isLocked = col.querySelector('i').classList.contains('fa-lock')
         const text = col.querySelector('h2')
         const button = col.querySelector('button')
         // const color = generateRandomColor()
         const color = chroma.random() // chroma генерирует разные цвета с помощью метода рандом
+
+        if (isLocked) { // останавливает выполнение функции и дальше не идёт, выполняется return
+            return
+        }
 
         text.textContent = color
         col.style.background =  color //  меняем бакграунд
